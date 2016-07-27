eba7c7a6a55258a86647c9e80c375121a94ed8ee
root <root@ip-172-31-10-121.ec2.internal>
2016-07-27 12:20:34 +0000

    Fourth Commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World");
-  res.send("Here comes my first build");
+  res.send("Hello World! Here comes my first build");
 });
 
 app.listen(4000);
