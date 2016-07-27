08cf1f4c6e96a4d831eaaf106ff2775165e43477
root <root@ip-172-31-10-121.ec2.internal>
2016-07-27 13:22:46 +0000

    last push made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hey Bengaluru! - 1');
+  res.send('Hey Bengaluru! - 2');
 });
 
 app.listen(3000, function () {
